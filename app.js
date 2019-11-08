ee3ff629d4a5351ce642ff19f456699863bef87e
Maria Andersson <monstermaria@gmail.com>
2019-11-08 12:47:31 +0100

    Added description of the task objects

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // this is the JavaScript file
 
+/* The task objects will have the following structure:
+{
+	taskDescription: "The task at hand",
+	finished: false
+}
+*/
+
 var tasks= [];
+
 function toggelFinished() {
 	document.getElementById("tasks").innerHTML = "true";
 	 
