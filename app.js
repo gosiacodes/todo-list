7206570c3f798d647614b550f18c80abcd65fe6f
margareta75 <margareta.xx@gmail.com>
2019-11-11 22:22:31 +0100

    Revert "vet inte vad det är men måste commit för att annars funkar inte pull"
    This reverts commit 513e536759622d5c6084f39eaee80301ae2f99b6.

--- app.js
+++ app.js
@@ -84,18 +84,10 @@ function rewriteTaskList() {
     }
 }
 
+
 function removeFinishedTasks() {
-    
-$(".strike-through").slideUp(funktion() {
-    $("this").remove();
-});
-}
-
-
-/*function removeFinishedTasks() {
     $(".strike-through").remove();
-     $(".strike-through").val("");
-}*/
+}
 
 /*function removeFinishedTasks() {
     for (var i = 0; i < tasks.length; i++) {
